76a7edf5c76b2983f78a7d4a7a032a89ffcb5ef0
amitsinghgh19 <prasadmanigh19>
2019-06-09 20:08:52 -0400

    added products into products tabel

--- Seeds.sql
+++ Seeds.sql
@@ -3,12 +3,14 @@ SELECT * FROM products;
 -- Add data to the table
 INSERT into products (product_name, department_name, price, stock_quantity)
 VALUES ("Samsung LED UHD TV", "Electronics", 1099.99, 10),
+("Samsung Cellphone", "Electronics", 399.99, 10),
+("Apple Cellphone", "Electronics", 999.99, 50),
 ("Laptop Dell", "Electronics", 599.58, 10),
 ("Laptop Apple", "Electronics", 999.58, 20),
-("Cellphone", "Electronics", 350.00, 25),
-("Hanes white t-shirts five-pack", "Clothing", 14.99, 30),
+("Hanes White T-Shirts Five-Pack", "Clothing", 14.99, 30),
 ("Aveeno Body Lotion", "Health & Beauty", 12.60, 35),
 ("AXE DEO For Men", "Health & Beauty", 4.99, 40),
 ("DOVE DEO For Women", "Health & Beauty", 5.60, 40),
+("Raisin Bran 3 Count", "Food & Grocery", 10.60, 40),
 ("Puppy-Licious Dog Biscuits", "Pet Supplies", 7.99, 100),
 ("Coding for Dummies", "Books", 20.99, 100);
